acbfbf587c6755307731ef9b15b7cf5cbbd69027
Bernar <67651190+BeKaZHoLdYbEk@users.noreply.github.com>
2026-05-14 16:00:25 +0800

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,202 +1,223 @@
-{ config, lib, pkgs, ... }:
+{self, inputs, ... }: {
 
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-  
-  nixpkgs.config.allowUnfree = true;
+    flake.nixosModules.myMachineConfiguration = { config, lib, pkgs, ... }: {
+      imports = [
+          self.nixosModules.myMachineHardware
+          self.nixosModules.niri
 
-  
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+          ];
 
-  programs.hyprland.enable = true;
- 
-  services.printing.enable = false;
-  services.touchegg.enable = true;
+      nixpkgs.config.allowUnfree = true;
+      nixpkgs.config.cudaSupport = false;
 
-  services.gnome.gnome-keyring.enable = true;
-  programs.seahorse.enable = true;
-  security.pam.services.login.enableGnomeKeyring = true;
-  services.dbus.enable = true;
-
-
-  # networking.hostName = "nixos"; # Define your hostname.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  hardware.enableRedistributableFirmware = true;
-
-  time.timeZone = "Asia/Kuala_Lumpur";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = false;
-
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "ru, us, kz";
-  # services.xserver.xkb.options = "grp:alt_shift_toggle";
-
-
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    alsa.enable = true;
-    wireplumber.enable = true;
-  };
-
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;     # Bluetooth tray + manager
-  services.power-profiles-daemon.enable = false;
-  services.upower.enable = true;
-  services.tlp.enable = true;
-
-  services.libinput.enable = true;
-
-
-  users.users.bernar = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
-  };
-
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    brave
-    hyprland
-    hyprpaper
-    waybar
-    kitty
-    vscode
-    steam
-    wofi
-    python313Packages.meson
-    python313Packages.cmake
-    scdoc
-    kdePackages.wayland-protocols  
-  
-    networkmanagerapplet
-    brightnessctl
-    pavucontrol
-    mako
-    grim
-    slurp
-    wl-clipboard
-    pamixer
-    playerctl
-    mpvpaper
-    mpv
-   
-    kdePackages.dolphin
-    kdePackages.qtsvg
-    kdePackages.kio-extras
-    p7zip
-    xwinwrap
-    
-  ];
-  programs.steam.enable = true;
-  programs.hyprlock.enable = true;
-  services.hypridle.enable = true;
-
-
-  
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-  ];
+      # Use the systemd-boot EFI boot loader.
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
 
 
 
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      vaapiVdpau
-    ];
-  };
+      services.gnome.gnome-keyring.enable = true;
+      programs.seahorse.enable = true;
+      security.pam.services.login.enableGnomeKeyring = true;
+      services.dbus.enable = true;
 
-  services.xserver.videoDrivers = [ "amdgpu" "nvidia" ];
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = true;                      # Open NVIDIA kernel module (recommended for RTX)
-    powerManagement.enable = true;
-    powerManagement.finegrained = true;
-  };
 
-  hardware.nvidia.prime = {
-    offload = {
-      enable = true;
-    };
-    nvidiaBusId = "PCI:1:0:0";
-    amdgpuBusId = "PCI:4:0:0";
-  };
+      networking.hostName = "bekazh"; # Hostname.
+      hardware.enableRedistributableFirmware = true;
+      time.timeZone = "Asia/Kuala_Lumpur";
 
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  };
+
+
+      services.pipewire = {
+          enable = true;
+          pulse.enable = true;
+          alsa.enable = true;
+          wireplumber.enable = true;
+      };
+
+
+
+      # --- Programs ---
+
+      # programs.hyprland.enable = true;
+      services.printing.enable = false;
+      services.touchegg.enable = true;
+      networking.networkmanager.enable = true;
+
+
+      hardware.bluetooth.enable = true;
+      hardware.bluetooth.powerOnBoot = true;
+      services.blueman.enable = true;     # Bluetooth tray + manager
+
+      services.power-profiles-daemon.enable = false;
+      services.upower.enable = true;
+      services.tlp.enable = true;
+      services.libinput.enable = true;
+
+      virtualisation.waydroid.enable = true;
 
 
 
 
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+      services.displayManager.defaultSession = "niri";
 
-  # List services that you want to enable:
+      nix.settings.trusted-users = [ "root" "bernar" ];
+      services.getty.autologinUser = "bernar";
+      users.users.bernar = {
+          isNormalUser = true;
+          extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+          packages = with pkgs; [
+          tree
+          ];
+      };
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+      xdg.mime.defaultApplications = {
+          "application/pdf" = "firefox.desktop";
+          "text/x-python" = "code.desktop";
+          "text/plain" = "kate.desktop";
+      };
+      xdg.portal = {
+          enable = true;
+          extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+          config.common.default = [ "gtk" ];
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+          # ADD THIS LINE: It tells Niri to use the GTK picker specifically.
+          #config.niri.default = [ "gtk" ];
+      };
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+      services.gvfs.enable = true; # Core support for MTP/Storage
+      services.udisks2.enable = true;
+      programs.thunar = {
+        enable = true;
+        plugins = with pkgs.xfce; [
+            thunar-volman # Auto-management of removable drives
+        ];
+        };
+      # 3. CRITICAL: Thunar needs Xfconf to remember settings & handle mounting
+        programs.xfconf.enable = true;
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.05"; # Did you read the comment?
+        # 4. Give yourself permission to mount (Polkit)
+        security.polkit.enable = true;
 
+
+
+      environment.systemPackages = with pkgs; [
+            jmtpfs
+            # --- Core Utilities ---
+            vim
+            wget
+            p7zip
+            networkmanagerapplet
+            wl-clipboard
+
+            # --- Development ---
+            gcc
+            gdb
+            glib
+            gsettings-desktop-schemas
+            binutils
+            devenv
+
+            # --- GUI Apps ---
+            firefox
+            vscode
+            vesktop
+            obs-studio
+            kitty
+            vlc
+            mpv
+            libreoffice
+            inkscape
+            gimp
+            kdePackages.kate
+            obsidian
+
+            # --- Niri/Wayland Essentials ---
+            brightnessctl
+            pavucontrol
+            grim
+            slurp
+            pamixer
+            playerctl
+            nordzy-cursor-theme
+            xwayland-satellite
+
+            thunar                     # Lightweight file manager
+            thunar-archive-plugin      # Lets you right-click to extract files
+            lxmenu-data                # Keeps your "Open With" menus working
+            shared-mime-info           # Helps apps identify file types
+
+            # --- Eye Candy ---
+            cmatrix
+            cava
+            mpvpaper
+        ];
+
+        programs.steam.enable = true;
+        programs.localsend.enable = true;
+        programs.kdeconnect.enable = true;
+        fonts.packages = with pkgs; [
+            nerd-fonts.jetbrains-mono
+        ];
+
+
+
+
+      programs.xwayland.enable = true;
+      # --- Graphics ---
+
+      hardware.graphics = {
+          enable = true;
+          enable32Bit = true;
+          extraPackages = with pkgs; [
+          libva-vdpau-driver
+          ];
+      };
+
+
+      services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
+      hardware.nvidia.open = true;
+
+
+      # ADD THIS LINE: This is strictly required for Xwayland on Nvidia
+      hardware.nvidia.modesetting.enable = true;
+
+
+      hardware.nvidia.prime = {
+          offload = {
+          enable = true;
+          enableOffloadCmd = true; };
+
+          amdgpuBusId = "PCI:4:0:0"; # integrated
+          nvidiaBusId = "PCI:1:0:0"; # dedicated
+      };
+
+
+      environment.sessionVariables = {
+          # Nvidia Fixes
+          WLR_NO_HARDWARE_CURSORS = "1";
+          __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+          LIBVA_DRIVER_NAME = "nvidia";
+          NVD_BACKEND = "direct";
+
+          # Session Identity
+          XDG_CURRENT_DESKTOP = "niri";
+          XDG_SESSION_TYPE = "wayland";
+
+          # Fix for GTK apps and themes
+          GTK_USE_PORTAL = "1";
+          XCURSOR_THEME = "Nordzy-cursors";
+          XCURSOR_SIZE = "24";
+          XDG_MENU_PREFIX = "plasma-";
+      };
+
+      programs.dconf.enable = true;
+
+
+      system.stateVersion = "25.05";
+      nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+      };
 }
